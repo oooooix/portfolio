d43010077167ccd2505cb7e40dceb032c15215ef
kaibabzmy <kaibabzmy@zhangmengyaodeMacBook-Pro.local>
2022-07-27 11:11:20 +0800

    Update UA to GA

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -16,19 +16,27 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-GH8J5DN8JX",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Defers execution of google analytics script after page load
-        defer: false,
-        cookieDomain: "none",
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-GH8J5DN8JX", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://www.mengyaozhang.me",
+        },
       },
     },
   ],
